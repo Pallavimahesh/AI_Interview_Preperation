945028c3f6be7e6ffd754d9801d320f633b27ca2
Pallavi M <pallavi.mahesh98@gmail.com>
2026-06-18 20:50:58 +0530

    Added request logger middlware

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const errorHandler = require("./middlewares/errorHandler");
-
+const requestLogger = require("./middlewares/requestLogger");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
@@ -11,7 +11,9 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-
+app.use(requestLogger);
 app.use("/api/auth", authRoutes);
+
 app.use(errorHandler);
+
 module.exports = app;
